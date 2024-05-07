1c9475c0eebcef289e5d5373418555aa529cd64c
Kylan Hurt <kylan.hurt@gmail.com>
2024-05-07 19:46:23 -0300

    Some styling improvements

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,11 +1,24 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
+import { useMatch, useLocation } from "react-router-dom";
+
+// convert path to title
+const getTitle = (path) => {
+  return path.replace(/\//g, '').replace(/-/g, ' ').replace(/\b\w/g, l => l.toUpperCase())
+}
 
 export default function Layout() {
+  const location = useLocation()
+  const match = useMatch(location.pathname)
+
+  console.log(location, match)
+
   return (
-    <div className="App">
+    <div className="App" style={{ backgroundColor: '#141618' }}>
       <Header />
-      <div style={{ height: '50px'}} />
+      <div style={{ borderBottom: '1px solid #777', padding: '0 48px' }}>
+        <h1 style={{ color: 'white' }}>{getTitle(match.pathname) || 'Dashboard'}</h1>
+      </div>
       <Outlet />
     </div>
   )
