b63d041eea0e0254c70955723b1085c245908741
Kylan Hurt <kylan.hurt@gmail.com>
2024-05-07 18:57:55 -0300

    Start implementing navigation

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
-import Header from "./Components/Header";
 import Stamp from "./Components/Stamp";
 import LoadingCircle from "./Components/Loader";
+import { Routes, Route, Outlet, Link } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { WagmiProvider } from 'wagmi' 
 import { config } from "./wagmi";
 import './App.css';
+import Layout from "./Components/Layout";
+import { BrowserRouter } from "react-router-dom";
 
 const queryClient = new QueryClient()
 
 function App() {
   return (
     <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}> 
-        <div className="App">
-          <Header />
-          <div style={{ height: '50px'}} />
-          <Stamp
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<div>hi</div>} />
+              {/* <Route path="about" element={<About />} />
+              <Route path="dashboard" element={<Dashboard />} />
+
+              {/* Using path="*"" means "match anything", so this route
+                    acts like a catch-all for URLs that we don't have explicit
+                    routes for. 
+              <Route path="*" element={<NoMatch />} /> */}
+            </Route>
+          </Routes>
+        </BrowserRouter>
+          {/* <Stamp
               points={8}
               title="Ownership verification"
               subtitle="Verify ownership of the smart contract address."
           />
-          <LoadingCircle />
-        </div>
+          <LoadingCircle /> */}
       </QueryClientProvider>
     </WagmiProvider>
   );
