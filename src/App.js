ff56de2914addd725a10b47de570fe6c72f8338d
Kylan Hurt <kylan.hurt@gmail.com>
2024-05-07 19:16:49 -0300

    Get basic navigation for each page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ import { config } from "./wagmi";
 import './App.css';
 import Layout from "./Components/Layout";
 import { BrowserRouter } from "react-router-dom";
+import Dashboard from "./Pages/Dashboard";
+import Activity from "./Pages/Activity";
+import Earn from "./Pages/Earn";
+import Redeem from "./Pages/Redeem";
+import Benefits from "./Pages/Benefits";
+import Support from "./Pages/Support";
 
 const queryClient = new QueryClient()
 
@@ -17,14 +23,17 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Layout />}>
-              <Route index element={<div>hi</div>} />
-              {/* <Route path="about" element={<About />} />
-              <Route path="dashboard" element={<Dashboard />} />
+              <Route index element={<Dashboard />} />
+              <Route path="activity" element={<Activity />} />
+              <Route path="earn" element={<Earn />} />
+              <Route path="redeem" element={<Redeem />} />
+              <Route path="benefits" element={<Benefits />} />
+              <Route path="support" element={<Support />} />
 
               {/* Using path="*"" means "match anything", so this route
                     acts like a catch-all for URLs that we don't have explicit
-                    routes for. 
-              <Route path="*" element={<NoMatch />} /> */}
+                    routes for. */ }
+              {/* <Route path="*" element={<NoMatch />} /> */}
             </Route>
           </Routes>
         </BrowserRouter>
