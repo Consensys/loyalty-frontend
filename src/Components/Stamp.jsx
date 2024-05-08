a403de99115e91e926b0f637b1cf0f6c4e84ab0a
Kylan Hurt <kylan.hurt@gmail.com>
2024-05-08 16:12:17 -0300

    Change stamp css to module

--- src/Components/Stamp.jsx
+++ src/Components/Stamp.jsx
@@ -1,19 +1,19 @@
-import '../Styles/Stamp.scss';
+import styles from '../Styles/Stamp.module.scss';
 import Solar from "../Images/Solar.svg";
 
 function Stamp({ points, title, subtitle, buttonText }) {
   return (
-    <div className="Stamp">
-      <div className="first-row">
+    <div className={`Stamp ${styles.Stamp}`}>
+      <div className={styles.firstRow}>
         <img src={Solar} />
-        <div className='claim-points-wrapper'>
-          <div className='claim-points-content'>+{points} XPs</div>
+        <div className={styles.claimPointsWrapper}>
+          <div className={styles.claimPointsContent}>+{points} XPs</div>
         </div>
       </div>
-      <div className="title">{ title }</div>
+      <div className={styles.title}>{ title }</div>
       <div>{ subtitle }</div>
-      <div className="button-wrapper">
-        <button className="button-submit">
+      <div className={styles.buttonsWrapper}>
+        <button className={styles.buttonSubmit}>
             <img src="/images/lock.png" />
             <span>{buttonText}</span>
         </button>
