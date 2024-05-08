5a6248442d547645aa2a69e7587755bc0e6ec3da
Kylan Hurt <kylan.hurt@gmail.com>
2024-05-07 22:05:44 -0300

    Start building dashboard page

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,7 +1,23 @@
-export default function () {
+import { Button } from '@mui/material'
+import '../Styles/Dashboard.css'
+
+const Dashboard = () => {
   return (
-    <div>
-      <h1>Dashboard</h1>
+    <div className="hero" >
+      <div className="info">
+        <h2>Start by Proving You're a Web3 Builder</h2>
+        <h3>Prove your identity to Mint your membership NFT</h3>
+        <ul>
+          <p><span className="number">1</span>Connect to GitHub to verify your code contributions</p>
+          <p><span className="number">2</span>Verify activity on Ethereum or Linea</p>
+          <p><span className="number">3</span>Mint your membership NFT.</p>
+        </ul>
+        <button className='button' onClick={null}>
+          Mint Your Membership NFT
+        </button>
       </div>
+    </div>
   )
 }
+
+export default Dashboard
