82be6249edc0e5b17aa7a2949a8b00aa5cc41e1e
Kylan Hurt <kylan.hurt@gmail.com>
2024-05-08 14:21:18 -0300

    Work on Dashboard Hero and lower 'Stamps'

--- src/Components/Stamp.js
+++ src/Components/Stamp.js
@@ -1,22 +1,25 @@
-import './Stamp.css';
+import './Stamp.scss';
 import Solar from "../Images/Solar.svg";
 
-function Stamp({ points, title, subtitle }) {
-    return (
-        <div className="Stamp">
-            <div className="first-row">
-                <img src={Solar} />
-                <div className='claim-points-wrapper'>
-                    <div className='claim-points-content'>Claim {points} points</div>
-                </div>
-            </div>
-            <div className="title">{ title }</div>
-            <div>{ subtitle }</div>
-            <div className="button-wrapper">
-                <button className="button-submit">Submit proof</button>
-            </div>
+function Stamp({ points, title, subtitle, buttonText }) {
+  return (
+    <div className="Stamp">
+      <div className="first-row">
+        <img src={Solar} />
+        <div className='claim-points-wrapper'>
+          <div className='claim-points-content'>+{points} XPs</div>
         </div>
-    );
+      </div>
+      <div className="title">{ title }</div>
+      <div>{ subtitle }</div>
+      <div className="button-wrapper">
+        <button className="button-submit">
+            <img src="/images/lock.png" />
+            <span>{buttonText}</span>
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Stamp;
