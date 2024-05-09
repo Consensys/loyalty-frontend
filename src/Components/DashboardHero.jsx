f94461a36aa421777301f22b936907645dbfaa97
Kylan Hurt <kylan.hurt@gmail.com>
2024-05-09 18:43:18 -0300

    WIP Web3BuilderVerification modal

--- src/Components/DashboardHero.jsx
+++ src/Components/DashboardHero.jsx
@@ -1,6 +1,24 @@
+import { Dialog, DialogContent } from '@mui/material'
 import styles from '../Styles/Dashboard.module.scss'
+import { useState } from 'react'
+import Web3BuilderVerification from './Web3BuilderVerification'
+import { useAccount, useConnect } from 'wagmi'
 
 export const DashboardHero = () => {
+  const [isModalVisible, setIsModalVisible] = useState(false)
+  const { isConnected } = useAccount()
+  const { connect, connectors } = useConnect()
+  const connector = connectors[0]
+
+  const onClickJoin = () => {
+    console.log('onClickJoin', isConnected)
+    if (!isConnected) {
+      connect({ connector })
+    } else {
+      setIsModalVisible(true)
+    }
+  }
+
   return (
     <div className={styles.hero} >
     <div className={styles.info}>
@@ -11,8 +29,8 @@ export const DashboardHero = () => {
         <p><span className="number">2</span>Verify activity on Ethereum or Linea</p>
         <p><span className="number">3</span>Mint your membership NFT.</p>
       </ul>
-      <button className={styles.button} onClick={null}>
-      Join the Build for MetaMask Program Now!
+      <button className={styles.button} onClick={onClickJoin}>
+        Join the Build for MetaMask Program Now!
       </button>
     </div>
     <div className={styles.cards}>
@@ -29,6 +47,14 @@ export const DashboardHero = () => {
         style={{ zIndex: 104, position: 'relative' }}
       />
     </div>
+    <Dialog
+      open={isModalVisible}
+      onClose={() => setIsModalVisible(false)}
+    >
+      <DialogContent className="modal">
+        <Web3BuilderVerification setIsVisible={setIsModalVisible} />
+      </DialogContent>
+    </Dialog>    
   </div>    
   )
 }
