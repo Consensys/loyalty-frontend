4f85886f11fa12f4bca55e47aeb284a670f52e31
witmicko <mjogrodniczak@gmail.com>
2024-05-09 14:00:35 +0100

    initial drawer, auditor fixes
    improve drawer
    
    improve drawer, improve auditor flow
    
    improve drawer, improve auditor flow
    
    improve drawer, improve auditor flow
    
    More adjustments to ContractOwnershipForm
    
    WIP Start implementing HTTP requests for ContractOwnershipForm
    
    Change one ContractOwnershipForm fetch to GET request
    
    WIP Web3BuilderVerification modal
    
    migrate to modal

--- src/Components/StampRow.jsx
+++ src/Components/StampRow.jsx
@@ -1,73 +1,79 @@
+
 import React, { useState } from 'react';
 import Stamp from './Stamp';
 import styles from '../Styles/Dashboard.module.scss'
 import ContractOwnershipForm from './ContractOwnershipForm';
 import { Dialog, DialogContent } from '@mui/material';
-
+import VerifyAuditDrawer from "./VerifyAuditDrawer"
+import AuditVerificationForm from "./AuditVerificationForm"
 
 const StampRow = () => {
-  const [modalIsVisible, setModalIsVisible] = useState(false);
-  const [modalType, setModalType] = useState('');
-  
+  const [modalIsVisible, setModalIsVisible] = useState(false)
+  const [modalType, setModalType] = useState("")
+
   const openModal = (type) => {
-    setModalType(type);
-    setModalIsVisible(true);
-  }
-  
-  const COMPONENTS = {
-    'ContractOwnershipForm': <ContractOwnershipForm setIsVisible={setModalIsVisible} />,
+    setModalType(type)
+    setModalIsVisible(true)
   }
 
-  const currentModalComponent = modalType ? COMPONENTS[modalType] : null;
+  const COMPONENTS = {
+    ContractOwnershipForm: <ContractOwnershipForm setIsVisible={setModalIsVisible} />,
+    AuditVerificationForm: <AuditVerificationForm setIsVisible={setModalIsVisible} />,
+  }
+
+  const currentModalComponent = modalType ? COMPONENTS[modalType] : null
+
+  const [verifyAuditIsOpen, setVerifyAuditIsOpen] = useState(false)
+
+  const toggleVerifyAuditDrawer = (open) => () => {
+    setVerifyAuditIsOpen(open)
+  }
+  
 
   return (
     <>
-        <div className={styles.cardWrap}>
+      <div className={styles.cardWrap}>
         <Stamp
-          onClick={() => openModal('ContractOwnershipForm')}
+          onClick={() => openModal("ContractOwnershipForm")}
           points={10}
           title="Prover Ownership of Dapp Smart-Contract"
           subtitle="Prove that you own your Dapp Smart-Contract."
-          buttonText='Verify ownership'
+          buttonText="Verify ownership"
         />
-        </div>
-        <div className={styles.cardWrap}>
-          <Stamp
-            onClick={() => openModal('ContractOwnershipForm')}
-            points={10}
-            title="Verify Ownership of Dapp Domain"
-            subtitle="Prove that your own your Dapp Domain URL."
-            buttonText='Verify ownership'
-          />
-        </div>
-        <div className={styles.cardWrap}>
-          <Stamp
-            onClick={() => openModal('ContractOwnershipForm')}
-            points={10}
-            title="Verify Smart-Contract Audit"
-            subtitle="Prove that your Dapp smart-contract has been audited."
-            buttonText='Submit proof'
-          />
-        </div>
-        <div className={styles.cardWrap}>
-          <Stamp
-            onClick={() => openModal('ContractOwnershipForm')}
-            points={10}
-            title="Verify Integration of MetaMask SDK"
-            subtitle="Submit proof of integration with the MetaMask SDK."
-            buttonText='Submit proof'
-          />
-        </div>
-        <Dialog
-          open={modalIsVisible}
-          onClose={() => setModalIsVisible(false)}
-        >
-          <DialogContent className="modal">
-            {currentModalComponent}
-          </DialogContent>
-        </Dialog>
+      </div>
+      <div className={styles.cardWrap}>
+        <Stamp
+          onClick={() => openModal("ContractOwnershipForm")}
+          points={10}
+          title="Verify Ownership of Dapp Domain"
+          subtitle="Prove that your own your Dapp Domain URL."
+          buttonText="Verify ownership"
+        />
+      </div>
+      <div className={styles.cardWrap}>
+        <Stamp
+          points={10}
+          title="Verify Smart-Contract Audit"
+          subtitle="Prove that your Dapp smart-contract has been audited."
+          buttonText="Submit proof"
+          onClick={() => openModal("AuditVerificationForm")}
+        />
+        <VerifyAuditDrawer isOpen={verifyAuditIsOpen} toggleDrawer={toggleVerifyAuditDrawer} />
+      </div>
+      <div className={styles.cardWrap}>
+        <Stamp
+          onClick={() => openModal("ContractOwnershipForm")}
+          points={10}
+          title="Verify Integration of MetaMask SDK"
+          subtitle="Submit proof of integration with the MetaMask SDK."
+          buttonText="Submit proof"
+        />
+      </div>
+      <Dialog open={modalIsVisible} onClose={() => setModalIsVisible(false)}>
+        <DialogContent className="modal">{currentModalComponent}</DialogContent>
+      </Dialog>
     </>
-  );
-};
+  )
+}
 
-export default StampRow;
+export default StampRow
--- src/wagmi.js
+++ src/wagmi.js
@@ -3,7 +3,7 @@ import { sepolia, mainnet, lineaSepolia } from "wagmi/chains"
 import { injected, metaMask, safe } from 'wagmi/connectors'
 
 export const wagmiConfig = createConfig({
-  chains: [mainnet, sepolia],
+  chains: [mainnet, sepolia, lineaSepolia],
   connectors: [
     injected(),
     safe(),
