29f937f15d7011a41edc530b036b272420867ec1
Kylan Hurt <kylan.hurt@gmail.com>
2024-05-09 15:20:11 -0300

    More adjustments to ContractOwnershipForm

--- src/Components/ContractOwnershipForm.jsx
+++ src/Components/ContractOwnershipForm.jsx
@@ -1,6 +1,7 @@
+import axios from 'axios';
 import React, { useState } from 'react';
 
-export default function ContractOwnershipForm() {
+export default function ContractOwnershipForm({ setIsVisible }) {
   const [formData, setFormData] = useState({
     dappName: '',
     smartContractAddress: '',
@@ -14,8 +15,20 @@ export default function ContractOwnershipForm() {
     })
   }
 
+  const onSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const { data } = await axios.post()
+      setIsVisible(false)
+    } catch (error){
+      console.error(error)
+    } finally {
+      // do something
+    }
+  }
+
   return (
-    <form style={{ width: 300}}>
+    <form style={{ width: 300}} onSubmit={onSubmit}>
       <h2 className="title">Prove Ownership of<br />Dapp Smart-Contract</h2>
       <p>Prove that you own your Dapp smart contract</p>
       <p className="instructions">Enter your dapp smart contract address below to start the verification process</p>
