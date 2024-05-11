1e6357fa53cbbada493a188833cd977153ed8fd9
Kylan Hurt <kylan.hurt@gmail.com>
2024-05-11 18:19:47 -0300

    Attempt fix header and CORS issue

--- src/constants.js
+++ src/constants.js
@@ -3,6 +3,5 @@ export const FETCH_CONFIG = {
     'X-API-KEY': '375262ce-c3d0-4609-a325-ec2831a071b4',
     accept: 'application/json',
     'ngrok-skip-browser-warning': 'skip-browser-warning',
-    "Access-Control-Allow-Origin": "*",
   }
 }
