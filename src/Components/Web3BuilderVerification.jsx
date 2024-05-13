7a194557ea713d3393f4e1db33db76ec0be194e0
Danica Shen <zhaodanica@gmail.com>
2024-05-13 14:39:20 +0100

    implement mint for step 3

--- src/Components/Web3BuilderVerification.jsx
+++ src/Components/Web3BuilderVerification.jsx
@@ -1,25 +1,36 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useAccount, useConnect } from "wagmi";
 import Snackbar from "@mui/material/Snackbar";
+import Alert from "@mui/material/Alert";
 import styles from "../Styles/Web3BuilderVerification.module.scss";
 import Cubes from "../Images/cubes.svg";
-import { FETCH_CONFIG, API_ENDPOINT } from "../constants";
-import { useToastHook } from "../Context/UseToastHook";
+import { FETCH_CONFIG, LEVELS_IDS, PHOSPHOR_API_KEY } from "../constants";
 import Close from "../Images/close.svg";
 import Check from "../Images/check.svg";
 
+const API_ENDPOINT = "https://0117-109-255-0-100.ngrok-free.app";
+
 export default function Web3BuilderVerification({ setIsVisible }) {
   const { isConnected, address } = useAccount();
-  const { isToastOpen, handleOpenToast, handleCloseToast } = useToastHook();
+
+  const [isToastOpen, setIsToastOpen] = useState(false);
+
+  const handleOpenToast = () => setIsToastOpen(true);
+
+  const handleCloseToast = () => setIsToastOpen(false);
+
   const [toastMessage, setToastMessage] = useState("");
+  const [toastType, setToastType] = useState("");
 
   const [githubAccountValue, setGithubAccountValue] = useState("");
   const [isGithubVerified, setIsGithubVerified] = useState(false);
   const [isGithubVerifyOpen, setIsGithubVerifyOpen] = useState(false);
 
   const [isEthereumVerified, setIsEthereumVerified] = useState(false);
+  const [isMintLoading, setIsMintLoading] = useState(false);
   const [isMembershipNFTMinted, setIsMembershipNFTMinted] = useState(false);
+  const [nftTransactionId, setNFTTransactionId] = useState(undefined);
 
   const stopEventBubbling = (event) => {
     event.stopPropagation();
@@ -38,22 +49,22 @@ export default function Web3BuilderVerification({ setIsVisible }) {
 
     const VerifyGithubAccount = async (event) => {
       stopEventBubbling(event);
-      // try {
-      //   const {
-      //     data,
-      //   } = await axios.post(
-      //     'http://f3ae-109-255-0-100.ngrok-free.app/v1/stamps/git/verify/0xc2326247DFf1e185874DC22CE7A26eFcF7FC39f3/user/dddddanica',
-      //     FETCH_CONFIG,
-      //   );
-      //   console.log({ data })
-      // setToastMessage("GitHub verification succeeded")
-      // } catch (err) {
-      // setToastMessage("GitHub verification failed. Please try again.")
-      // handleOpenToast();
-      // }
-      // TODO: Only on success
-      setIsGithubVerified(true);
-      setIsGithubVerifyOpen(false);
+      try {
+        await axios.post(
+          `${API_ENDPOINT}/v1/stamps/git/verify/${address}/user/${githubAccountValue}`,
+          FETCH_CONFIG,
+        );
+        setToastType("success");
+        setToastMessage("GitHub verification succeeded !");
+        handleOpenToast();
+        setIsGithubVerified(true);
+        setIsGithubVerifyOpen(false);
+      } catch (err) {
+        setToastType("error");
+        setToastMessage("GitHub verification failed. Please try again.");
+        handleOpenToast();
+      }
+      setTimeout(() => handleCloseToast(), 5000);
     };
 
     return (
@@ -81,24 +92,131 @@ export default function Web3BuilderVerification({ setIsVisible }) {
     );
   };
 
-  const handleVerifyEth = (event) => {
+  const handleVerifyEth = async (event) => {
     stopEventBubbling(event);
-    // try {
-    //   const {
-    //     data,
-    //   } = await axios.post(
-    //     'http://f3ae-109-255-0-100.ngrok-free.app/v1/stamps/eth/verify/0x95b3045f2daaA24e864D4983EA7c6CcbF5C3da1C',
-    //     FETCH_CONFIG,
-    //   );
-    //   console.log({ data })
-    //   setToastMessage("Verify Ethereum activity verification succeeded")
-    // } catch (err) {
-    // setToastMessage("Verify Ethereum activity verification failed. Please try again.")
-    // handleOpenToast();
-    // }
-    // TODO: Only on success
-    setIsEthereumVerified(true);
+    try {
+      await axios.post(
+        `${API_ENDPOINT}/v1/stamps/eth/verify/${address}`,
+        FETCH_CONFIG,
+      );
+      setToastType("success");
+      setToastMessage("Ethereum activity succeeded !");
+      handleOpenToast();
+      setIsEthereumVerified(true);
+    } catch (err) {
+      setToastType("failure");
+      setToastMessage("GitHub verification failed. Please try again.");
+      handleOpenToast();
+    }
+    setTimeout(() => handleCloseToast(), 5000);
   };
+
+  const handleMintNFT = (event) => {
+    stopEventBubbling(event);
+    setIsMintLoading(true);
+    axios
+      .post(
+        "https://admin-api.phosphor.xyz/v1/mint-requests",
+        {
+          item_id: LEVELS_IDS.EXPLORER,
+          quantity: "1",
+          to_address: address,
+        },
+        {
+          headers: {
+            "Phosphor-Api-Key": PHOSPHOR_API_KEY,
+          },
+        },
+      )
+      .then(function (response) {
+        const { data } = response;
+        const { mint_requests } = data;
+        const { transaction_id } = mint_requests[0];
+        setNFTTransactionId(transaction_id);
+      })
+      .catch(function (error) {
+        setToastMessage("Mint your NFT failed. Please try again.");
+        handleOpenToast();
+      });
+    setTimeout(() => handleCloseToast(), 5000);
+  };
+
+  useEffect(() => {
+    if (nftTransactionId) {
+      function fetchNFTTransaction(attempt = 1) {
+        if (attempt > 5) {
+          // Set a maximum number of attempts to avoid infinite loops
+          console.error("Maximum attempts reached. Stopping fetch.");
+          return;
+        }
+
+        axios
+          .get(
+            `https://admin-api.phosphor.xyz/v1/transactions/${nftTransactionId}`,
+            {
+              headers: {
+                "Phosphor-Api-Key": PHOSPHOR_API_KEY,
+              },
+            },
+          )
+          .then((response) => {
+            if (
+              response.data &&
+              response.data["on_chain_status"] === "SUCCESS"
+            ) {
+              setToastType("success");
+              setToastMessage("NFT transaction succeeded !");
+              handleOpenToast();
+              console.log("NFT transaction successful!");
+
+              setIsMintLoading(false);
+              setIsMembershipNFTMinted(true);
+            } else {
+              setToastType("info");
+              handleOpenToast();
+              setToastMessage("Minting NFT transaction...");
+              console.log(
+                `NFT transaction not successful yet. Attempt ${attempt}`,
+              );
+              setTimeout(() => fetchNFTTransaction(attempt + 1), 8000); // Retry after delay
+            }
+          })
+          .catch((error) => {
+            setToastType("error");
+            setToastMessage("Fetching NFT transaction. Please try again.");
+            handleOpenToast();
+          });
+      }
+
+      // Start the initial fetch
+      fetchNFTTransaction();
+    }
+  }, [nftTransactionId]);
+
+  useEffect(() => {
+    // isEthereumVerified && &&  isMembershipNFTMinted
+    // if (isGithubVerified) {
+    //   try {
+    //     axios.post(`${API_ENDPOINT}/v1/user/level`, FETCH_CONFIG);
+    //     axios
+    //       .get(`${API_ENDPOINT}/level/1`, FETCH_CONFIG)
+    //       .then((data) => console.log({ data }));
+    //     setToastType("success");
+    //     setToastMessage("GitHub verification succeeded !");
+    //     handleOpenToast();
+    //     setIsGithubVerified(true);
+    //     setIsGithubVerifyOpen(false);
+    //   } catch (err) {
+    //     setToastType("error");
+    //     setToastMessage("GitHub verification failed. Please try again.");
+    //     handleOpenToast();
+    //   }
+    //   setTimeout(() => handleCloseToast(), 5000);
+    //   // save to db http://f3ae-109-255-0-100.ngrok-free.app/v1/user/level
+    //   // Show NFT as the poker card:
+    //   // http://f3ae-109-255-0-100.ngrok-free.app/v1/level/1
+    // }
+  }, [isEthereumVerified, isGithubVerified, isMembershipNFTMinted]);
 
   return (
     <form className={styles.web3BuilderVerification}>
@@ -171,28 +289,47 @@ export default function Web3BuilderVerification({ setIsVisible }) {
       <div className={styles.divider} />
       <div className={styles.step}>
         <div className={styles.stepWrapper}>
-          <div className={styles.stepNumber}>3</div>
+          {isMembershipNFTMinted ? (
+            <img src={Check} className={styles.checkIcon} />
+          ) : (
+            <div className={styles.stepNumber}>3</div>
+          )}
           <div className={styles.subtitle}>
             Join the Build for MetaMask program by making your membership NFT
           </div>
         </div>
-        <button
-          className={`${styles.smallBtn} small-btn`}
-          variant="contained"
-          color="primary"
-          type="submit"
-        >
-          Mint
-        </button>
+        {!isMembershipNFTMinted && (
+          <button
+            className={`${styles.smallBtn} small-btn`}
+            variant="contained"
+            color="primary"
+            type="submit"
+            onClick={handleMintNFT}
+          >
+            {isMintLoading ? "Loading..." : "Mint"}
+          </button>
+        )}
       </div>
+      <div className={styles.divider} />
+      {/*<div className={styles.finalWelldone}>*/}
+      {/*  Well done, you just minted your first Build For MetaMask Membership NFT!*/}
+      {/*</div>*/}
       <Snackbar
         anchorOrigin={{ vertical: "top", horizontal: "center" }}
         open={isToastOpen}
         autoHideDuration={3000}
         onClose={handleOpenToast}
-        message={toastMessage}
-        action={<img src={Close} />}
-      />
+        action={<img src={Close} onClick={handleCloseToast} />}
+      >
+        <Alert
+          onClose={handleCloseToast}
+          severity={toastType}
+          variant="filled"
+          sx={{ width: "100%" }}
+        >
+          {toastMessage}
+        </Alert>
+      </Snackbar>
     </form>
   );
 }
--- src/constants.js
+++ src/constants.js
@@ -7,3 +7,12 @@ export const FETCH_CONFIG = {
 };
 
 export const API_ENDPOINT = "https://f3ae-109-255-0-100.ngrok-free.app/v1/";
+
+export const LEVELS_IDS = {
+  EXPLORER: "f3a33c73-015b-4ba0-a175-5d08f1a30600",
+  SILVER: "8348f99a-c707-4cbb-b387-4a66813bedb1",
+  GOLD: "012b48cd-9ff2-4e9e-a449-aef3ea003a97",
+  ULTIMATE: "7034f3a2-4a99-405b-8a99-54c9d9d11346",
+};
+
+export const PHOSPHOR_API_KEY = "784404a7b0624c1eba3e816bd9c5ab44";
